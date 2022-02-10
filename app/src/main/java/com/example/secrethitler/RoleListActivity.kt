11d99cbbd757abec77552bdc5b575180ba7cd5c9
Oleg <oleg.cortuzz1@gmail.com>
2022-02-11 02:14:56 +0700

    Roles are showing to players

--- app/src/main/java/com/example/secrethitler/RoleListActivity.kt
+++ app/src/main/java/com/example/secrethitler/RoleListActivity.kt
@@ -3,17 +3,39 @@ package com.example.secrethitler
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcel
+import android.view.View
+import android.widget.TextView
 import java.io.Serializable
 
 class RoleListActivity : AppCompatActivity()
 {
-    private lateinit var game: Serializable
+    private lateinit var game: Game
+    private var playerCount = 0
+    private var isShowingCards = false
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_role_list)
 
-        game = intent.getSerializableExtra("game")!!
+        game = intent.getSerializableExtra("game")!! as Game
+        val playerName = game.getPlayerNameByIndex(playerCount)
+        findViewById<TextView>(R.id.playerName).text = playerName +  "'s is"
+    }
+
+    fun nextScreen(view: View) {
+        val playerName = game.getPlayerNameByIndex(playerCount)
+        val playerRole = game.getPlayerRole(playerName)
+
+        if (isShowingCards) {
+            findViewById<TextView>(R.id.playerName).text = playerName +  "'s is"
+            isShowingCards = false
+        }
+        else {
+            findViewById<TextView>(R.id.playerName).text =
+                playerName +  "'s is " + playerRole.toString()
+            isShowingCards = true
+            playerCount += 1
+        }
     }
 }
