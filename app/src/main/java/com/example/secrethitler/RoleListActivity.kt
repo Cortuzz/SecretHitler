24891d3559f9dc0146755de23ffc96bc416af9dd
Oleg <oleg.cortuzz1@gmail.com>
2022-02-11 09:45:50 +0700

    Roles images are showing to players

--- app/src/main/java/com/example/secrethitler/RoleListActivity.kt
+++ app/src/main/java/com/example/secrethitler/RoleListActivity.kt
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcel
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import java.io.Serializable
 
 class RoleListActivity : AppCompatActivity()
 {
     private lateinit var game: Game
+    private val rolesImagesIds = mapOf<Role, Int>(Role.LIBERAL to R.id.imageLiberal,
+    Role.FASCIST to R.id.imageFascist, Role.HITLER to R.id.imageHitler)
+
     private var playerCount = 0
     private var isShowingCards = false
 
@@ -20,20 +24,29 @@ class RoleListActivity : AppCompatActivity()
 
         game = intent.getSerializableExtra("game")!! as Game
         val playerName = game.getPlayerNameByIndex(playerCount)
-        findViewById<TextView>(R.id.playerName).text = playerName +  "'s is"
+        findViewById<TextView>(R.id.playerName).text = playerName +  " is"
+    }
+
+    fun hideRoles() {
+        for ((role, id) in rolesImagesIds) {
+            findViewById<ImageView>(id).visibility = View.INVISIBLE
+        }
     }
 
     fun nextScreen(view: View) {
         val playerName = game.getPlayerNameByIndex(playerCount)
         val playerRole = game.getPlayerRole(playerName)
+        val roleImage = findViewById<ImageView>(rolesImagesIds[playerRole]!!)
 
         if (isShowingCards) {
-            findViewById<TextView>(R.id.playerName).text = playerName +  "'s is"
+            hideRoles()
+            findViewById<TextView>(R.id.playerName).text = playerName +  " is"
             isShowingCards = false
         }
         else {
+            roleImage.visibility = View.VISIBLE
             findViewById<TextView>(R.id.playerName).text =
-                playerName +  "'s is " + playerRole.toString()
+                playerName +  " is " + playerRole.toString()
             isShowingCards = true
             playerCount += 1
         }
